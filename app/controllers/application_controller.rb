c4a9adb72431573883c0345ca5e7d75237f65127
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-04-12 12:01:49 +0000

    チェックシート21　ルート変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     about_path
   end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  
 
   protected
 
